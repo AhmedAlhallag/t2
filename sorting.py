17107f12492d7b5523d4a0c3033c1f3414ca1314
ah <aalhallag@gmail.com>
2020-12-06 16:33:09 +0400

    ADDED Sorting classs

---
+++ sorting.py
@@ -0,0 +1,30 @@
+class Sorting():
+
+    def __init__(self):
+        self.nums = None
+
+    def set_list(self, nums):
+        self.nums = nums
+
+    def bubble_sort(self):
+        for i in range(len(self.nums)):
+            swap = False
+            for j in range(len(self.nums)-i-1):
+                if self.nums[j] > self.nums[j+1]:
+                    temp = self.nums[j]
+                    self.nums[j] = self.nums[j+1]
+                    self.nums[j+1] = temp
+                    swap = True
+            if not swap:
+                break
+
+    def get_list(self):
+
+        return self.nums
+
+
+# sortObj = Sorting()
+
+# sortObj.set_list([-1, 20, -5, 2])
+# sortObj.bubble_sort()
+# print(sortObj.get_list())
